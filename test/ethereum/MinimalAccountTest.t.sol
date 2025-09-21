67f51c48d47ae549cde2e1043213cfff499771c8
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-21 16:17:50 +0530

    added test

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -7,17 +7,19 @@ import {DeployMinimal} from "../../script/DeployMinimal.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {MinimalAccount} from "../../src/ethereum/MinimalAccount.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-//import {SendPackedUserOp, PackedUserOperation, IEntryPoint} from "script/SendPackedUserOp.s.sol";
+import {SendPackedUserOp, PackedUserOperation, IEntryPoint} from "script/SendPackedUserOp.s.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
 
 contract MinimalAccountTest is Test {
+    using MessageHashUtils for bytes32;
+
     MinimalAccount minimalAccount;
     DeployMinimal deployMinimal;
     HelperConfig helperConfig;
     ERC20Mock usdc;
-    // SendPackedUserOp sendPackedUserOp;
+    SendPackedUserOp sendPackedUserOp;
 
     address randomuser = makeAddr("randomUser");
 
@@ -28,6 +30,7 @@ contract MinimalAccountTest is Test {
 
         (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
         usdc = new ERC20Mock();
+        sendPackedUserOp = new SendPackedUserOp();
     }
 
     function testOwnerCanExecuteCommands() public {
@@ -54,5 +57,25 @@ contract MinimalAccountTest is Test {
         vm.prank(randomuser);
         vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
         minimalAccount.execute(dest, value, functionData);
+    }
+
+    function testRecoverSignedOp() public {
+        // Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+        bytes memory executeCallData =
+            abi.encodeWithSelector(MinimalAccount.execute.selector, dest, value, functionData);
+        PackedUserOperation memory packedUserOp = sendPackedUserOp.generateSignedUserOperation(
+            executeCallData, helperConfig.getConfig(), address(minimalAccount)
+        );
+        bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+
+        // Act
+        address actualSigner = ECDSA.recover(userOperationHash.toEthSignedMessageHash(), packedUserOp.signature);
+
+        // Assert
+        assertEq(actualSigner, minimalAccount.owner());
     }
 }
